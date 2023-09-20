0de217642e0978a8d44e9623efefe4288209b82c
quangthanh1995 <17130213@st.hcmuaf.edu.vn>
2023-09-21 02:44:37 +0700

    sdfsdff

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Udacity2023
-export POSTGRES_HOST=database-1.cnutrd1kdjx3.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=myPassword
+export POSTGRES_HOST=
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-thanhnq10-udagram
+export AWS_BUCKET=arn:aws:s3:::myawsbucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://myawsbucket-thanhnq10-udagram.s3-website-us-east-1.amazonaws.com/
+export URL=http://localhost:8100
