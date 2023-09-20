b1bb80a3accccfaf6008a80c3be28f0627c3aaa1
quangthanh1995 <17130213@st.hcmuaf.edu.vn>
2023-09-21 01:21:41 +0700

    edit env v1

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Udacity2023
-export POSTGRES_HOST=
+export POSTGRES_HOST=database-1.cnutrd1kdjx3.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::myawsbucket-thanhnq10-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://myawsbucket-thanhnq10-udagram.s3-website-us-east-1.amazonaws.com/
