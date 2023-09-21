584a6f7bbebd92e3af5d0f8f65508acc9f1a8a9a
quangthanh1995 <17130213@st.hcmuaf.edu.vn>
2023-09-21 23:23:48 +0700

    run code locally successfully

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
@@ -54,7 +54,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Start the Server
   app.listen(port, () => {
-    console.log(`Backend server is listening on port ${process.env.PORT}....`);
+    console.log(`Backend server is listening on port ${port}....`);
     console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
   });
